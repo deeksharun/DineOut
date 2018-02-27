0ef828f7cf219860389e2fa34d2a7ba882f3b8b5
Deeksha <tyss@localhost>
2018-02-27 09:45:14 +0530

    Second commit

---
+++ src/demo_prj/sample.java
@@ -0,0 +1,12 @@
+package demo_prj;
+
+import org.testng.annotations.Test;
+
+public class sample {
+	@Test
+	public void hello() {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+	}
+
+}
